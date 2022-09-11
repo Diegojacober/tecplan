1c83ac99a4e02b5780b98e1c29b772cff4728fdd
Diego <diegoalencar.jacober@gmail.ocm>
2022-09-11 13:33:58 -0300

    Testando pwa

---
+++ sw.js
@@ -0,0 +1,45 @@
+var CACHE_NAME = 'static-v1';
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll([
+        '/index.html',
+        '/css/style.css',
+        '/estufa-control.html',
+        '/tela-de-cadastro.html',
+        '/reset-password.html',
+        '/login.html',
+        'forget-password.html',
+        '/home.html',
+        'js/app.js',
+        'js/ajax.js',
+        'js/jquery.js',
+        'js/sweetalert.js'
+      ]);
+    })
+  )
+});
+
+self.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys
+        .filter(function (key) {
+          return key.indexOf(CACHE_NAME) !== 0;
+        })
+        .map(function (key) {
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
